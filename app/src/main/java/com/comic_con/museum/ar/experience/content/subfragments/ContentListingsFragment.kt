fb68ccb92f7df95579b04ece8a0aec49d47ef8db
Christians, Ben <bbc7909@rit.edu>
2018-11-29 14:25:40 -0800

    wip?

---
+++ app/src/main/java/com/comic_con/museum/ar/experience/content/subfragments/ContentListingsFragment.kt
@@ -0,0 +1,15 @@
+package com.comic_con.museum.ar.experience.content.subfragments
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ContentListingsFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+//        val thisView = inflater.inflate()
+        return null
+    }
+}
---
+++ app/src/main/java/com/comic_con/museum/ar/experience/content/subfragments/ContentOverviewFragment.kt
@@ -0,0 +1,7 @@
+package com.comic_con.museum.ar.experience.content.subfragments
+
+import android.support.v4.app.Fragment
+
+class ContentOverviewFragment: Fragment() {
+
+}
